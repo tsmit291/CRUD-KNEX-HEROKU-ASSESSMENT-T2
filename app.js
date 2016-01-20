bf4ae91bdd08f4ad93d5ff7a09814fde8a59793d
marthaberner <marthaberner.mb@gmail.com>
2016-01-19 23:23:35 -0700

    test posts CRUD

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var posts = require('./routes/posts');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/posts');
+  res.json({'SUCCESS': 'add your database results here'});
 });
 
 module.exports = router;
