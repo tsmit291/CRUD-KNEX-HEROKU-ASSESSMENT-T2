d154e61c129a7d51d7ebe6b8e5d4414eed0d8deb
= <tsmit291@icloud.com>
2016-01-20 12:57:21 -0700

    working on test_post_comments

--- routes/comments.js
+++ routes/comments.js
@@ -2,5 +2,40 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
 
+function Comments(){
+  return knex('comments');
+};
+
+function Posts(){
+  return knex('posts');
+};
+
+router.get('/:post_id/comments', function(req, res, next){
+    Comments().where('post_id', req.params.post_id).then(function(comments){
+      res.json({'SUCCESS': comments});
+  });
+});
+
+router.post('/:post_id/comments', function(req, res, next){
+  Comments().insert(req.body).first().then(function(comments){
+    res.redirect('/'+ req.params.post_id + '/comments/');
+  });
+});
+
+router.get('/:post_id/comments/:id', function(req, res, next){
+  Comments().where('id', req.params.id).first().then(function(comments){
+    res.json({'SUCCESS': comments});
+  });
+});
+
+router.get('/:post_id/comments/:id/edit', function(req, res, next){
+  Comments().where('id', req.params.id).first().then(function(comments){
+    res.json({'SUCCESS': comments});
+  });
+});
+
+
+
+
 
 module.exports = router;
--- test/test_post_comments.js
+++ test/test_post_comments.js
@@ -11,12 +11,12 @@ describe('Post Comments CRUD Routes', function() {
   beforeEach(function () {
     return knex.seed.run(knex.config);
   });
-  xit('should use knex migrations', function () {
+  it('should use knex migrations', function () {
   return knex('knex_migrations').select().then(function (migrations) {
     assert.equal(migrations.length, 2);
     })
   })
-  xit('should GET ALL comments for a post on /posts/:post_id/comments', function (done) {
+  it('should GET ALL comments for a post on /posts/:post_id/comments', function (done) {
     chai.request(server)
     .get('/posts').end(function (err, res) {
       var thePost=res.body.SUCCESS[1];
@@ -32,7 +32,7 @@ describe('Post Comments CRUD Routes', function() {
       });
     })
   });
-  xit('should POST a post comment to /posts/:post_id/comments', function(done) {
+  it('should POST a post comment to /posts/:post_id/comments', function(done) {
     var theComment = {
       'commenter': 'Oscar Wilde',
       'body': 'Be yourself; everyone else is already taken.'
@@ -51,7 +51,7 @@ describe('Post Comments CRUD Routes', function() {
           });
       })
     });
-  xit('should GET a SINGLE post comment from /posts/:post_id/comments/:id', function(done) {
+  it('should GET a SINGLE post comment from /posts/:post_id/comments/:id', function(done) {
     chai.request(server)
       .get('/posts').end(function (err, res) {
         var thePost = res.body.SUCCESS[1];
